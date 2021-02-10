f6750804741f2ca840491a70deda146a0d4aacce
= <=>
2021-02-11 01:30:09 +0300

    update 6 task

--- 6/6-ext.js
+++ 6/6-ext.js
@@ -41,3 +41,53 @@ const king = new King("Cesar");
 pleb.go();
 pleb.eat();
 king.go();
+
+function ProtoExtentds() {
+    this.count = 0;
+    this.finish = 5;
+    Consumer.call(this);
+    this.go = function () {
+        if (this.count < this.finish) {
+            console.log("step: " + this.count);
+            this.count++;
+            this.go();
+        }
+    };
+
+    Object.setPrototypeOf(this, Consumer);
+}
+
+const counter = new ProtoExtentds();
+counter.go();
+
+class Entity2D {
+    mass = 1;
+    constructor(x = 0, y = 0, m = Entity2D.mass) {
+        this.x = x;
+        this.y = y;
+        this.mass = m;
+    }
+
+    pos = function () {
+        console.log("X: ", this.x, "\nY: ", this.y);
+    };
+
+    mass() {
+        return this.mass;
+    }
+}
+
+class Entity3D extends Entity2D {
+    mass = 3;
+    constructor(x = 0, y = 0, z = 0, m = Entity3D.mass) {
+        super(x, y, m);
+        this.z = z;
+    }
+
+    pos = function () {
+        console.log("X: ", this.x, "\nY: ", this.y, "\nZ: ", this.z);
+    };
+}
+
+const cat2d = new Entity2D(10, 10, 3);
+const dog3d = new Entity3D(0, 0, 100);
